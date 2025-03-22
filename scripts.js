b8982da3108df6423dbb9b80ff1b244efa17fb70
Krishna <krishnak511@gmail.com>
2025-03-22 02:36:29 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Typed.js Initialization
+document.addEventListener('DOMContentLoaded', function () {
+  var typed = new Typed('#typed', {
+    strings: ["Cybersecurity Engineer", "Business Developer", "Coder"],
+    typeSpeed: 50,
+    loop: true
+  });
+
+  // AOS Initialization
+  AOS.init();
+});
